172604651373a967e61ea3c29b875bd6a8abeeda
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-01-23 22:22:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from pyrogram import Client, filters
 
 tgbot=Client(
     "Pyrogram Bot",
-    bot_token="5024393552:AAERJgw_5mZ9csN5XBvnf8iZMzySfKg2lI4"
-    api_id="8050217"
+    bot_token="5024393552:AAERJgw_5mZ9csN5XBvnf8iZMzySfKg2lI4",
+    api_id="8050217",
     api_hash="8a733396605cf07c31dfc79d7245270d"
 )
 
