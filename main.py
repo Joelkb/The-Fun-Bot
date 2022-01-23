1b82da2aec4570fd7806e7b47c9076427f626f04
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-01-23 22:01:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from pyrogram import Client, filters
 
+tgbot=Client(
+    "Pyrogram Bot",
+    bot_token="5024393552:AAERJgw_5mZ9csN5XBvnf8iZMzySfKg2lI4"
+    api_id="8050217"
+    api_hash="8a733396605cf07c31dfc79d7245270d"
+)
+
+@tgbot.on_message(filters.command("start"))
+async def start_message(bot, message):
+    await message.reply_text("👋 Hello I'am a test bot made by <a href='https://t.me/creatorbeatz'>JOEL</a>")
+
+tgbot.run()
