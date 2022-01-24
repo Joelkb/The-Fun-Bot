383737a27885564060742ac19278bd9a1e7702bc
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-01-24 09:30:43 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,3 +1,5 @@
 START_TXT = """<b>HELLO I'AM A TEST BOT MADE BY <a href='https://t.me/creatorbeatz'><i>JOEL KURIAN BIJU</i></a>.
 
 <br>IF YOU WANT MORE DETAILS OR HELP, TRY SENDING /help COMMAND TO ME...</b>"""
+
+HELP_TXT = """<b>HELLO IF YOU NEED HELP, TRY CONTACTING MY DEVELOPER...</b>"""
