db4a4024a4d11818393e9a98c9b216277b8fc16c
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-02-02 13:59:29 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,4 +2,4 @@ START_TXT = """<b>HELLO I'AM A TEST BOT MADE BY <a href='https://t.me/creatorbea
 
 <br>IF YOU WANT MORE DETAILS OR HELP, TRY SENDING /help COMMAND TO ME...</b>"""
 
-HELP_TXT = """<b>HELLO IF YOU NEED HELP, TRY CONTACTING MY DEVELOPER...</b>"""
+HELP_TXT = """<b>HELLO IF YOU NEED HELP, TRY CONTACTING MY <a href='https://t.me/creatorbeatz'><u>DEVELOPER...</u></a></b>"""
