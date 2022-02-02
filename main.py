d2ac9ff2d1f68441d67525645bb7c3cf3da475da
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-02-02 23:00:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,6 +49,6 @@ async def howilook_message(bot, message):
 
 @tgbot.on_message(filters.command("dice"))
 async def dice_message(bot, message):
-    await message.reply_text("🎲")
+    await message.reply_emoji("🎲")
 
 tgbot.run()
