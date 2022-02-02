c9b55609f831e934ea74c469df98f5093f697c3d
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-02-02 14:54:04 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,3 +3,5 @@ START_TXT = """<b>HELLO I'AM A TEST BOT MADE BY <a href='https://t.me/creatorbea
 <br>IF YOU WANT MORE DETAILS OR HELP, TRY SENDING /help COMMAND TO ME...</b>"""
 
 HELP_TXT = """<b>HELLO IF YOU NEED HELP, TRY CONTACTING MY <a href='https://t.me/creatorbeatz'><u>DEVELOPER</u></a>...</b>"""
+
+LOOK_TXT = """<b>This is how you look 😂☝️</b>"""
