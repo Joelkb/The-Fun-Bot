f87b35ea4e22f086427c3fd3065890965c0a0647
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-02-02 12:32:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,3 +1,5 @@
-START_IMG = "https://telegra.ph/file/6a0726f79acd8300e9a04.jpg"
+from os import environ
+
+START_IMG = (environ.get('START_IMG', 'https://telegra.ph/file/6a0726f79acd8300e9a04.jpg https://telegra.ph/file/d799c1a964f211028cc97.jpg https://telegra.ph/file/708a1d6ce805fcc6a46d0.jpg https://telegra.ph/file/85f95494565a762edb3e7.jpg https://telegra.ph/file/8c34c755dd16581c1c6b5.jpg https://telegra.ph/file/b987425b80bca0cf45c7e.jpg https://telegra.ph/file/2a8b3779760289b76de24.jpg https://telegra.ph/file/47961be968719b3e24cf0.jpg https://telegra.ph/file/2e127b0f6b1810d733c09.jpg https://telegra.ph/file/fcc849db4bf5c517f0f8d.jpg')).split()
 
 HELP_IMG = "https://telegra.ph/file/365e35b554e5a3ea83857.jpg"
