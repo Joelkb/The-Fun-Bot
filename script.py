a57d9c2dcf77ee379539b0f5aacf6511f823be3f
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-02-09 17:01:55 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,37 +1,37 @@
-START_TXT = """<b>Hey {},
-<br>I'm a fun bot made by <a href='https://t.me/creatorbeatz'><i>Joel Kurian Biju</i></a>.
+START_TXT = """𝐇𝐞𝐲 <b>{},</b>
+<br>𝐈'𝐦 𝐚 𝐟𝐮𝐧 𝐛𝐨𝐭 𝐦𝐚𝐝𝐞 𝐛𝐲 <a href='https://t.me/creatorbeatz'><i>𝐉𝐨𝐞𝐥 𝐊𝐮𝐫𝐢𝐚𝐧 𝐁𝐢𝐣𝐮</i></a>.
 
-<br>Click on the 'ℹ️ Help' button and see my powers 🔥
+<br>𝐂𝐥𝐢𝐜𝐤 𝐨𝐧 𝐭𝐡𝐞 ℹ️ Help 𝐛𝐮𝐭𝐭𝐨𝐧 𝐚𝐧𝐝 𝐬𝐞𝐞 𝐦𝐲 𝐩𝐨𝐰𝐞𝐫𝐬 🔥
 
-<br>More features and fun are on the way ✨</b>"""
+<br>𝐌𝐨𝐫𝐞 𝐟𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐚𝐧𝐝 𝐟𝐮𝐧 𝐚𝐫𝐞 𝐨𝐧 𝐭𝐡𝐞 𝐰𝐚𝐲 ✨"""
 
-HELP_TXT = """<b>👋 Hey {},
-<br>Here Are My Commands.
+HELP_TXT = """👋 𝐇𝐞𝐲 <b>{},</b>
+<br>𝐇𝐞𝐫𝐞 𝐀𝐫𝐞 𝐌𝐲 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬.
 
-<br>• TRY SENDING /howilook COMMAND AND SEE HOW YOU LOOK 🔥
+<br>• 𝐓𝐑𝐘 𝐒𝐄𝐍𝐃𝐈𝐍𝐆 /howilook 𝐂𝐎𝐌𝐌𝐀𝐍𝐃 𝐀𝐍𝐃 𝐒𝐄𝐄 𝐇𝐎𝐖 𝐘𝐎𝐔 𝐋𝐎𝐎𝐊 🔥
 
-<br>• TRY SENDING /dice OR /roll COMMAND TO ROLL A DICE 🎲
+<br>• 𝐓𝐑𝐘 𝐒𝐄𝐍𝐃𝐈𝐍𝐆 /dice 𝐎𝐑 /roll 𝐂𝐎𝐌𝐌𝐀𝐍𝐃 𝐓𝐎 𝐑𝐎𝐋𝐋 𝐀 𝐃𝐈𝐂𝐄 🎲
 
-<br>• TRY SENDING /goal OR /shoot COMMAND TO KICK A FOOTBALL ⚽
+<br>• 𝐓𝐑𝐘 𝐒𝐄𝐍𝐃𝐈𝐍𝐆 /goal 𝐎𝐑 /shoot 𝐂𝐎𝐌𝐌𝐀𝐍𝐃 𝐓𝐎 𝐊𝐈𝐂𝐊 𝐀 𝐅𝐎𝐎𝐓𝐁𝐀𝐋𝐋 ⚽
 
-<br>• TRY SENDING /throw OR /dart COMMAND TO THROW AN ARROW 🎯
+<br>• 𝐓𝐑𝐘 𝐒𝐄𝐍𝐃𝐈𝐍𝐆 /throw 𝐎𝐑 /dart 𝐂𝐎𝐌𝐌𝐀𝐍𝐃 𝐓𝐎 𝐓𝐇𝐑𝐎𝐖 𝐀𝐍 𝐀𝐑𝐑𝐎𝐖 🎯
 
-<br>• TRY SENDING /fun COMMAND FOR MORE FUN 💥
+<br>• 𝐓𝐑𝐘 𝐒𝐄𝐍𝐃𝐈𝐍𝐆 /pinball 𝐎𝐑 /tenpin 𝐂𝐎𝐌𝐌𝐀𝐍𝐃 𝐓𝐎 𝐏𝐋𝐀𝐘 𝐏𝐈𝐍𝐁𝐀𝐋𝐋 🎳
 
-<br>• TRY SENDING /pinball OR /tenpin COMMAND TO PLAY PINBALL 🎳
+<br>• 𝐓𝐑𝐘 𝐒𝐄𝐍𝐃𝐈𝐍𝐆 /luck 𝐎𝐑 /cownd 𝐓𝐎 𝐓𝐄𝐒𝐓 𝐘𝐎𝐔𝐑 𝐋𝐔𝐂𝐊 🎰
 
-<br>• TRY SENDING /luck OR /cownd TO TEST YOUR LUCK 🎰
+<br>• 𝐓𝐑𝐘 𝐒𝐄𝐍𝐃𝐈𝐍𝐆 /fun 𝐂𝐎𝐌𝐌𝐀𝐍𝐃 𝐅𝐎𝐑 𝐌𝐎𝐑𝐄 𝐅𝐔𝐍 💥
 
-<br>MORE FEATURES AND FUN ARE ON THE WAY ✨
+<br>𝐌𝐎𝐑𝐄 𝐅𝐄𝐀𝐓𝐔𝐑𝐄𝐒 𝐀𝐍𝐃 𝐅𝐔𝐍 𝐀𝐑𝐄 𝐎𝐍 𝐓𝐇𝐄 𝐖𝐀𝐘 ✨
 
-<br>BOT MADE BY <a href='https://t.me/creatorbeatz'><i>JOEL KURIAN BIJU</i></a></b>"""
+<br>BOT MADE BY <a href='https://t.me/creatorbeatz'><i>JOEL KURIAN BIJU</i></a>"""
 
-LOOK_TXT = """<b>This is how {} looks like 😂☝️
+LOOK_TXT = """𝐓𝐡𝐢𝐬 𝐢𝐬 𝐡𝐨𝐰 <b>{}</b> 𝐥𝐨𝐨𝐤𝐬 𝐥𝐢𝐤𝐞 😂☝️
 
-<br>MORE FEATURES ARE ON THE WAY ✨</b>"""
+<br>𝐌𝐎𝐑𝐄 𝐅𝐄𝐀𝐓𝐔𝐑𝐄𝐒 𝐀𝐑𝐄 𝐎𝐍 𝐓𝐇𝐄 𝐖𝐀𝐘 ✨"""
 
-ABOUT_TXT = """<b>Hey {},
-These are my details...</b>
+ABOUT_TXT = """𝐇𝐄𝐘<b>{},</b>
+𝐓𝐡𝐞𝐬𝐞 𝐚𝐫𝐞 𝐦𝐲 𝐝𝐞𝐭𝐚𝐢𝐥𝐬...
 ✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: 𝚃𝙷𝙴 𝙵𝚄𝙽 𝙱𝙾𝚃
 ✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/creatorbeatz>𝙹𝙾𝙴𝙻 𝙺𝚄𝚁𝙸𝙰𝙽 𝙱𝙸𝙹𝚄</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
