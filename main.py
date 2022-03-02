d9e8848193e11f0cdd265496cf33db5a0330fcd0
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-03-02 20:37:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -456,7 +456,7 @@ async def runs(_, message):
     else:
         await message.reply_text(effective_string)
 
-@tgbot.on_message(filters.all("Hi"))
+@tgbot.on_filter.text("Hi")
 async def filter_handler(bot, message):
     await message.reply_text("Hello, How are you?")
 
