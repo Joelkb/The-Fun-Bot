ec318d96ae2e35799e7ae61ce4e6cf90482bcb9d
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-03-04 20:14:12 +0530

    Update Filters.py

--- Plugins/Filters.py
+++ Plugins/Filters.py
@@ -3,7 +3,7 @@ from info import MOVIE_PIC
 from scrypt import MOVIE_ENG_TXT
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-@Client.on_message(filters.regex("movie") | filters.regex("Movie"))
+@tgbot.on_message(filters.regex("movie") | filters.regex("Movie"))
 async def filter_handler(bot, message):
     await message.reply_photo(
             photo=(MOVIE_PIC),
