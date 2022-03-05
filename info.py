699cd4adfb005c4709e0232572e689e993a12959
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-03-05 21:37:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,4 +1,14 @@
 from os import environ
+import re
+
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
 
 START_IMG = (environ.get('START_IMG', 'https://telegra.ph/file/6a0726f79acd8300e9a04.jpg https://telegra.ph/file/d799c1a964f211028cc97.jpg https://telegra.ph/file/708a1d6ce805fcc6a46d0.jpg https://telegra.ph/file/85f95494565a762edb3e7.jpg https://telegra.ph/file/8c34c755dd16581c1c6b5.jpg https://telegra.ph/file/b987425b80bca0cf45c7e.jpg https://telegra.ph/file/2a8b3779760289b76de24.jpg https://telegra.ph/file/47961be968719b3e24cf0.jpg https://telegra.ph/file/2e127b0f6b1810d733c09.jpg https://telegra.ph/file/fcc849db4bf5c517f0f8d.jpg')).split()
 
