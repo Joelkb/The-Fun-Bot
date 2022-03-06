ba34e6a31ba722c3a41de88578f5d465d61248fa
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-03-06 13:43:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from info import START_IMG, COMMAND_HAND_LER, MOVIE_PIC, ADMINS
+from info import START_IMG, LOOK_IMG, COMMAND_HAND_LER, MOVIE_PIC, ADMINS
 from script import START_TXT, HELP_TXT, LOOK_TXT, ABOUT_TXT, SOURCE_TXT, MAL_TRAN, HIN_TRAN, LANG, OWNER_INFO, MOVIE_ENG_TXT, MOVIE_MAL_TXT
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 import random
