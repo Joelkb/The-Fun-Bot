8d46a7e560985450f5b836e26a6f7ee094871887
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-03-06 12:21:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,15 @@ tgbot=Client(
     api_hash="8a733396605cf07c31dfc79d7245270d"
 )
 
-logger = logging.logger
-logging.logger.basicConfig(filename='filename.log', level=logging.DEBUG,
-                    format=' %(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='Fun_Bot.log', encoding='utf-8', level=logging.DEBUG)
+
+@tgbot.on_message(filters.command('logs') & filters.user(ADMINS))
+async def log_file(bot, message):
+    """Send log file"""
+    try:
+        await message.reply_document('Fun_Bot.log')
+    except Exception as e:
+        await message.reply(str(e))
 
 
 @tgbot.on_message(filters.command("start"))
