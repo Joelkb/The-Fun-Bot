7e65503dfad3ee84b3e1ca0478cc23188ae15522
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-03-23 21:35:26 +0530

    Update sample_config.py

--- plugins/sample_config.py
+++ plugins/sample_config.py
@@ -10,9 +10,9 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "YOUR KEY HERE"
-    OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "YOUR USERNAME HERE"
+    API_KEY = "8050217"
+    OWNER_ID = "1177577143"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "@creatorbeatz"
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
