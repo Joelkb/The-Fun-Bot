08bf4dc76a5d085cc2ccb771b05bd7db8b82d19a
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-04-28 10:28:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from pyrogram import Client, filters
 from info import START_IMG, LOOK_IMG, COMMAND_HAND_LER, MOVIE_PIC, ADMINS, API_HASH, API_ID, BOT_TOKEN, MV_PIC, FSub_Channel, SESSION, VOICE
 from script import START_TXT, LOOK_TXT, HELP_TXT, ABOUT_TXT, SOURCE_TXT, MOVIE_ENG_TXT, MOVIE_MAL_TXT, OWNER_INFO, MV_TXT, KICKED, FSUB, COMMAND_USER, REPORT_TXT
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InlineQueryResultArticle, InputTextMessageContent
 from pyrogram.errors import UserNotParticipant
 from plugins.fun_strings import FUN_STRINGS
+from pyrogram.handlers import MessageHandler
+from pyshorteners import Shortener
 import random
+import aiohttp
 import os
 import asyncio
 
