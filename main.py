d74af823dcb5b16292e2ae129f4875572349841d
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-04-28 10:23:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -338,19 +338,29 @@ async def runs(_, message):
     else:
         await message.reply_text(effective_string)
 
-@tgbot.on_message(filters.regex("@admin") | filters.regex("@admins"))
-async def admin_handler(bot, message):
-    admins = await message.reply_sticker(
-            sticker='CAACAgUAAxkBAAEEMnhiNA722UYMtilQ36wzPU1QTWLZ7gACqQADyJRkFOv8RlMxwyrKIwQ',
-            reply_markup=InlineKeyboardMarkup(
-                      [[
-                        InlineKeyboardButton("✅ REPORT SENT ✅", callback_data="report")
-                      ]]
-            )
-)
-    await asyncio.sleep(20)
-    await admins.delete()
-    await message.delete()
+@tgbot.on_message((filters.command(["report"]) | filters.regex("@admins") | filters.regex("@admin")) & filters.group)
+async def report_user(bot, message):
+    if message.reply_to_message:
+        chat_id = message.chat.id
+        reporter = str(message.from_user.id)
+        mention = message.from_user.mention
+        admins = await bot.get_chat_members(chat_id=chat_id, filter="administrators")
+        success = True
+        report = f"𝖱𝖾𝗉𝗈𝗋𝗍𝖾𝗋 : {mention} ({reporter})" + "\n"
+        report += f"𝖬𝖾𝗌𝗌𝖺𝗀𝖾 : {message.reply_to_message.link}"
+        for admin in admins:
+            try:
+                reported_post = await message.reply_to_message.forward(admin.user.id)
+                await reported_post.reply_text(
+                    text=report,
+                    chat_id=admin.user.id,
+                    disable_web_page_preview=True
+                )
+                success = True
+            except:
+                pass
+        if success:
+            await message.reply_text("𝖱𝖾𝗉𝗈𝗋𝗍𝖾𝖽 𝗍𝗈 𝖠𝖽𝗆𝗂𝗇𝗌!")
 
 @tgbot.on_message(filters.command("dialogues"))
 def dialogue_handler(bot, message):
