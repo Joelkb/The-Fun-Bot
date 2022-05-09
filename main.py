d617e87a05ead9ecc81c676107dd6f01251bb6ba
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-05-09 09:58:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -364,4 +364,28 @@ async def share_text(client, message):
         return
     await message.reply_text(share_link(input_text), reply_to_message_id=reply_id)
 
+@tgbot.on_message((filters.command(["report"]) | filters.regex("@admins") | filters.regex("@admin")) & filters.group)
+async def report_user(bot, message):
+    if message.reply_to_message:
+        chat_id = message.chat.id
+        reporter = str(message.from_user.id)
+        mention = message.from_user.mention
+        admins = await bot.get_chat_members(chat_id=chat_id, filter="administrators")
+        success = True
+        report = f"𝖱𝖾𝗉𝗈𝗋𝗍𝖾𝗋 : {mention} ({reporter})" + "\n"
+        report += f"𝖬𝖾𝗌𝗌𝖺𝗀𝖾 : {message.reply_to_message.link}"
+        for admin in admins:
+            try:
+                reported_post = await message.reply_to_message.forward(admin.user.id)
+                await reported_post.reply_text(
+                    text=report,
+                    chat_id=admin.user.id,
+                    disable_web_page_preview=True
+                )
+                success = True
+            except:
+                pass
+        if success:
+            await message.reply_text(f"Hey {message.from_user.mention}, Your Report Has Been Sent T𝗈 𝖠𝖽𝗆𝗂𝗇𝗌!")    
+
 tgbot.run()
