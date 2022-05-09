0dcf331c47ba709c15bd09ae44aababb83c98236
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-05-09 09:18:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from script import START_TXT, LOOK_TXT, HELP_TXT, ABOUT_TXT, SOURCE_TXT, MOVIE_E
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram.errors import UserNotParticipant
 from plugins.fun_strings import FUN_STRINGS
-from urllib.parse import quote
 import random
 import os
 import asyncio
@@ -341,36 +340,6 @@ async def runs(_, message):
 
 @tgbot.on_message(filters.command("dialogues"))
 def dialogue_handler(bot, message):
-    tgbot.send_voice(message.chat.id, random.choice(VOICE), caption="<b>Join @filmy_harbour</b>")
-
-SUPPORT_CHAT = "filmy_harbour_support"
-
-def share_link(text: str) -> str:
-    return "**Here is Your Sharing Text 👇**\n\nhttps://t.me/share/url?url=" + quote(text)
-
-@Client.on_message(filters.command(["share", "sharetext", "st", "stxt", "shtxt", "shtext"]))
-async def share_text(client, message):
-    reply = message.reply_to_message
-    reply_id = message.reply_to_message.message_id if message.reply_to_message else message.message_id
-    input_split = message.text.split(None, 1)
-    if len(input_split) == 2:
-        input_text = input_split[1]
-    elif reply and (reply.text or reply.caption):
-        input_text = reply.text or reply.caption
-    else:
-        await message.reply_text(
-            text=f"**Notice:**\n\n1. Reply Any Messages.\n2. No Media Support\n\n**Any Question Join Support Chat**",
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            "Support Chat", url=f"https://t.me/{SUPPORT_CHAT}")
-                    ]                
-                ]
-            ),
-            reply_to_message_id=reply_id
-        )
-        return
-    await message.reply_text(share_link(input_text), reply_to_message_id=reply_id)
+    await message.send_voice(message.chat.id, random.choice(VOICE), caption="<b>Join @filmy_harbour</b>")
 
 tgbot.run()
