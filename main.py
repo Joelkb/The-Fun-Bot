233a04786ccb3cbdbd0a424b12b80a94f837de36
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-05-09 09:35:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from pyrogram import Client, filters
-from info import START_IMG, LOOK_IMG, COMMAND_HAND_LER, MOVIE_PIC, ADMINS, API_HASH, API_ID, BOT_TOKEN, MV_PIC, FSub_Channel, SESSION, VOICE
-from script import START_TXT, LOOK_TXT, HELP_TXT, ABOUT_TXT, SOURCE_TXT, MOVIE_ENG_TXT, MOVIE_MAL_TXT, OWNER_INFO, MV_TXT, KICKED, FSUB, COMMAND_USER, REPORT_TXT
+from info import START_IMG, LOOK_IMG, COMMAND_HAND_LER, MOVIE_PIC, ADMINS, API_HASH, API_ID, BOT_TOKEN, MV_PIC, FSub_Channel, SESSION
+from script import START_TXT, LOOK_TXT, HELP_TXT, ABOUT_TXT, SOURCE_TXT, MOVIE_ENG_TXT, MOVIE_MAL_TXT, OWNER_INFO, MV_TXT, KICKED, FSUB, COMMAND_USER
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram.errors import UserNotParticipant
 from plugins.fun_strings import FUN_STRINGS
+from urllib.parse import quote
 import random
 import os
 import asyncio
 
+SUPPORT_CHAT = "filmy_harbour_support"
 
 tgbot=Client(
     session_name=SESSION,
@@ -212,11 +214,7 @@ async def cb_checker(bot, query: CallbackQuery):
                 caption=(MV_TXT),
                 reply_markup=reply_markup,
                 parse_mode='html'
-            )
-            
-        elif query.data == "report":
-            await query.answer(REPORT_TXT.format(query.from_user.first_name), show_alert=True)
-            
+            )           
 
 @tgbot.on_message(filters.command("howilook"))
 async def howilook_message(bot, message):
@@ -338,8 +336,32 @@ async def runs(_, message):
     else:
         await message.reply_text(effective_string)
 
-@tgbot.on_message(filters.command("dialogues"))
-async def dialogue_handler(bot, message):
-    await message.reply_voice(message.chat.id, random.choice(VOICE), caption="<b>Join @filmy_harbour</b>")
+def share_link(text: str) -> str:
+    return "**Here is Your Sharing Text 👇**\n\nhttps://t.me/share/url?url=" + quote(text)
+
+@tgbot.on_message(filters.command(["share", "sharetext", "st", "stxt", "shtxt", "shtext"]))
+async def share_text(client, message):
+    reply = message.reply_to_message
+    reply_id = message.reply_to_message.message_id if message.reply_to_message else message.message_id
+    input_split = message.text.split(None, 1)
+    if len(input_split) == 2:
+        input_text = input_split[1]
+    elif reply and (reply.text or reply.caption):
+        input_text = reply.text or reply.caption
+    else:
+        await message.reply_text(
+            text=f"**Notice:**\n\n1. Reply Any Messages.\n2. No Media Support\n\n**Any Question Join Support Chat**",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            "Support Chat", url=f"https://t.me/{SUPPORT_CHAT}")
+                    ]                
+                ]
+            ),
+            reply_to_message_id=reply_id
+        )
+        return
+    await message.reply_text(share_link(input_text), reply_to_message_id=reply_id)
 
 tgbot.run()
