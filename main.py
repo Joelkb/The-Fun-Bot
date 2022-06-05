db9cd7d10cbcf6ea87e17ebd823df5206b0d4514
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-06-05 09:16:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -589,14 +589,14 @@ async def trash_handler(bot, message):
             await message.reply_text("<b>Hey, Use this command as a reply to any message...</b>")
 
 @tgbot.on_message(filters.command("load"))
-async def load_handler(bot, query):
+async def load_handler(bot, message):
     for i in range(5):
-        await query.reply_text("<b>Started Loading</b>")
+        n = await message.reply_text("<b>Started Loading</b>")
         asyncio.sleep(1)
-        await query.message.edit_text("<b>Started Loading.</b>")
+        await n.edit_text("<b>Started Loading.</b>")
         asyncio.sleep(1)
-        await query.message.edit_text("<b>Started Loading..</b>")
+        await n.edit_text("<b>Started Loading..</b>")
         asyncio.sleep(1)
-        await query.message.edit_text("<b>Started Loading...</b>")
+        await n.edit_text("<b>Started Loading...</b>")
 
 tgbot.run()
