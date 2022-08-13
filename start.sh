7fe50cbe19564d615264c1e94a4bc4e8a030e8e8
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-08-13 19:41:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ git clone https://github.com/Joelkb/Beta-dq-the-file-donor-.git /The-Fun-Bot
 cd /The-Fun-Bot
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
-python3 bot.py
+python3 main.py
