99059fba286cb5aca9026cc0db59f19a6a143e24
Dev-f-k <112109531+Dev-f-k@users.noreply.github.com>
2023-03-04 11:25:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ git clone https://github.com/Dev-f-k/The-Fun-Bot.git /The-Fun-Bot
 cd /The-Fun-Bot
 pip3 install -U -r requirements.txt
 echo "Starting The-Fun-Bot..."
-python3 main.py
+python3 The-Fun-Bot/main.py
